a6d425c8fa35a0f0d3c2f495d002f6b0b0dc9b4e
Martin Wong <martin.wong@amethodschools.org>
2019-03-07 01:57:10 +0000

    added gif result

--- js/script.js
+++ js/script.js
@@ -4,7 +4,18 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+  $(".gallery").empty();
+  var custom_url="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+  var searched=$("#search-term").val();
+  $.ajax({
+      url:custom_url,
+      method:"GET",
+      success: function(response){
+        var randomizer = Math.floor(Math.random() * response.data.length);
+        $(".gallery").append("<img src='" + response.data[randomizer].images.fixed_width.url + "'/>");
+        
+      }
+  });
   
   
 });
