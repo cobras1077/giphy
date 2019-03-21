00ca96c81820fd1f15c938b2fbe002b5b27fce5b
Martin Wong <martin.wong@amethodschools.org>
2019-03-21 00:41:50 +0000

    more function buttons

--- js/script.js
+++ js/script.js
@@ -13,12 +13,63 @@ $("#search-button").click(function(){
       url:custom_url,
       method:"GET",
       success: function(response){
-        var randomizer = Math.floor(Math.random() * response.data.length);
-        $(".gallery").append("<img src='" + response.data[randomizer].images.fixed_width.url + "'/>");
+      var randomizer = Math.floor(Math.random() * response.data.length);
+      $(".gallery").append("<img src='" + response.data[randomizer].images.fixed_width.url + "'/>");
         
       }
   });
   
+  $("#emailLink").click(function(){
+  var searched=$("#search-term").val();
+  var custom_url="https://api.giphy.com/v1/gifs/search?q="+searched+"&rating=pg&api_key=dc6zaTOxFJmzC";
+  
+ 
+  $.ajax({
+      url:custom_url,
+      method:"GET",
+      success: function(response){
+      $('#emailLink').on('click', function (event) {
+      event.preventDefault();
+    alert("Huh");
+    var email = 'test@theearth.com';
+    var subject = 'Circle Around';
+    var emailBody = response.data[Math.floor(Math.random() * response.data.length)].images.fixed_width.url;
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+  });
+  
+      }
+      });
+  });
+  
+  
+});
+$("#random-button").click(function(){
+  $(".gallery").empty();
+  var custom_url= "https://api.giphy.com/v1/gifs/search?q=random&rating=pg&api_key=dc6zaTOxFJmzC";
+  $.ajax({
+      url:custom_url,
+      method:"GET",
+      success: function(response){
+      var randomizer = Math.floor(Math.random() * response.data.length);
+      $(".gallery").append("<img src='" + response.data[randomizer].images.fixed_width.url + "'/>");
+        
+      }
+  });
   
 });
 
+// $("#search-all").click(function(){
+//   $(".gallery").empty();
+//   var searched=$("#search-term").val();
+//   var custom_url="https://api.giphy.com/v1/gifs/search?q="+searched+"&rating=pg&api_key=dc6zaTOxFJmzC";
+  
+ 
+//   $.ajax({
+//       url:custom_url,
+//       method:"GET",
+//       success: function(response){
+//       var length= response.data
+//       var newHtml=[];
+//       }
+//   });
+// });
